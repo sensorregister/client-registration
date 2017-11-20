32e20c766f2f3bd2be8d51f61ade3e65bef9275d
Gerard Mulder <g.mulder@cgi.com>
2017-11-20 14:45:28 +0100

    Initial commit

---
+++ src/main/java/nl/kadaster/sensor/clientregistration/ClientRegistrationApplication.java
@@ -0,0 +1,12 @@
+package nl.kadaster.sensor.clientregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientRegistrationApplication.class, args);
+	}
+}
---
+++ src/test/java/nl/kadaster/sensor/clientregistration/ClientRegistrationApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.kadaster.sensor.clientregistration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClientRegistrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
