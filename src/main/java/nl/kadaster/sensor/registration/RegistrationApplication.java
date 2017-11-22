5c6c486eb93197fd1a894bcd7553722bf45089df
Raymond Kroon <raymond@k3n.nl>
2017-11-22 11:12:41 +0100

    eureka uit op dev

--- src/main/java/nl/kadaster/sensor/registration/RegistrationApplication.java
+++ src/main/java/nl/kadaster/sensor/registration/RegistrationApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 @EnableFeignClients
-@EnableDiscoveryClient
 public class RegistrationApplication {
 
 	public static void main(String[] args) {
